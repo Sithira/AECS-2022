458365e5f69f479426861c87254a688024a9abdf
Sithira Munasinghe <sithiraac@gmail.com>
2022-05-01 22:38:52 +0530

    completed docker-compose.yml for local development

--- services/dex-auth-service/src/main/java/me/sithiramunasinghe/services/authservice/DexAuthApplication.java
+++ services/dex-auth-service/src/main/java/me/sithiramunasinghe/services/authservice/DexAuthApplication.java
@@ -1,7 +1,11 @@
 package me.sithiramunasinghe.services.authservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class DexAuthApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(DexAuthApplication.class, args);
     }
 }
